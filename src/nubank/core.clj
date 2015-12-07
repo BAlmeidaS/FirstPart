f07319a11ed4ce1c4dbf99a0045f76910c527fd9
Bruno Almeida <Bruno.almeida.silveira@gmail.com>
2015-12-07 04:44:01 -0200

    inclusao das funcoes contém e search

--- src/nubank/core.clj
+++ src/nubank/core.clj
@@ -1,3 +1,4 @@
+
 (ns nubank.core
   (:require [clojure.java.io :as io] ))
 
@@ -6,28 +7,51 @@
 
 (def 	edges [])
 
-(defn 	teste
-		"aa"
+(defn 	not-equal [] (complement =))
+
+(defn 	contem
+		"Funcao que retorna se um numero x esta contido dentro de um vetor"
+		([x y] 
+			(if (= nil (some #(= x %) y)) false true)
+		)
+)
+
+
+(defn 	search
+		"Funcao que retorna um vetor com os vetores de ligacao de um no x"
+		([x] [])
+		([x y] (if (empty? y) (search x) 
+					(if (contem x (y 0))  
+						(conj (search (search x (subvec y 1)) x (subvec y 1)) (y 0))
+						
+
+						(search (search x (subvec y 1)) x (subvec y 1))	
+					)
+				)
+		)
+		([z x y] (if (empty? y) (search x y) z))
+	)
+
+(defn 	initEdges
+		"Função que inicia o vetor Edges com os valores do txt"
 		[]
 		(with-open [rdr (io/reader datafile)]
 			(doseq [line (line-seq rdr)]
-				(def edges (conj edges (clojure.string/split line #" ")))
+				(def edges (conj edges 
+						(into []	
+							(map #(Integer. %) 
+								(clojure.string/split line #" ")
+							)
+						)
+					)
+				)
 			)
 		)
 	)
 
-;(defn 	readFile
-;		"função para ler o arquivo edge"
-;		[]
-;		(
-;			with-open [rdr (reader "edges.txt")]
- ; 				(doseq [line (line-seq rdr)]
-  ;  				(println line))
-	;	)
-
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (teste)
-  (println edges)
+  (initEdges)
+  (println (search 2 edges))
   )
